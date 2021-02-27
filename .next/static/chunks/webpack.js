ee661b7c0c37126617c9c22960cf643c061233e2
WellingtonFidelis <wellingtonf20@gmail.com>
2021-02-26 23:00:42 -0300

    class 5

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c303c54188854b460350";
+/******/ 	var hotCurrentHash = "08f4a7947f955519f0e8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
